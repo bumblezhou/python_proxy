0145993b1f0ef2c9d8ad12b69aa52143bb30a305
bumble.zhou <bumble.zhou@gmail.com>
2020-03-26 09:46:45 +0800

    mime types

--- settings.py
+++ settings.py
@@ -1,9 +1,4 @@
 MIME_DICT = {
-    '.mpd': 'application/dash+xml',
-    '.sass': 'text/x-sass',
-    '.scss': 'text/x-scss',
-    '.less': 'text/less',
-    '.stylus': 'text/stylus',
-    '.styl': 'text/stylus',
-    '.apk': 'application/vnd.android.package-archive'
+    '.png': 'image/png',
+    '.jpg': 'image/jpg'
 }
